65b770733366d3631f963e82095416c6cbf6b7d5
Umar Saeed <umar23lhr@gmail.com>
2024-03-10 20:50:04 +0500

    Assignment 2

---
+++ Assignment 2/jsongenerator.py
@@ -0,0 +1,56 @@
+# Import statements
+
+
+def create_dict():
+    """ Creates a dictionary that stores an employee's information
+
+    1. Return a dictionary that maps "first_name" to name, "age" to age, and "title" to title
+
+    Args:
+        name: Name of employee
+        age: Age of employee
+        title: Title of employee
+
+    Returns:
+        dict - A dictionary that maps "first_name", "age", and "title" to the
+               name, age, and title arguments, respectively. Make sure that 
+               the values are typecasted correctly (name - string, age - int, 
+               title - string)
+    
+    WRITE YOUR SOLUTION BELOW
+    """
+
+
+    
+
+def write_json_to_file():
+    """ Write json string to file
+
+    1. Open a new file defined by output_file
+    2. Write json_obj to the new file
+
+    Args:
+        json_obj: json string containing employee information
+        output_file: the file the json is being written to
+     
+    WRITE YOUR SOLUTION BELOW
+    """
+
+    
+
+def main():
+    # Print the contents of details() -- This should print the details of an employee
+    
+
+    # Create employee dictionary
+   
+
+    # Use a function called dumps from the json module to convert employee_dict
+    # into a json string and store it in a variable called json_object.
+    
+
+    # Write out the json object to file
+    
+
+if __name__ == "__main__":
+    main()
